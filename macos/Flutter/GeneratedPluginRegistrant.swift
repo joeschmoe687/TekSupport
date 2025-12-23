11f13378625cfc60dcdfbb5414ad2641c07376e5
joeschmoe687 <tekneckjoe@gmail.com>
2025-12-22 23:27:24 -0600

    tekmate changes

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -21,7 +21,7 @@ import path_provider_foundation
 import record_macos
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
